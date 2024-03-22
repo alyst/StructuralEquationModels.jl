92fd99883e9bd03b899c4f044a23784f7555ab63
Alexey Stukalov <astukalol@seer.bio>
2024-03-22 15:12:14 -0700

    generic imply: impose matrix constraints
    define S, A, and I_A fields as symmetric or low/up triangular

--- src/imply/RAM/generic.jl
+++ src/imply/RAM/generic.jl
@@ -108,18 +108,17 @@ function RAM(;
     n_var = nvars(ram_matrices)
 
     #preallocate arrays
-    nan_params = fill(NaN, n_par)
-    A_pre = materialize(ram_matrices.A, nan_params)
-    S_pre = materialize(ram_matrices.S, nan_params)
+    rand_params = randn(Float64, n_par)
+    A_pre = check_acyclic(materialize(ram_matrices.A, rand_params))
+    S_pre = Symmetric(materialize(ram_matrices.S, rand_params))
     F = copy(ram_matrices.F)
 
-    A_pre = check_acyclic(A_pre, ram_matrices.A)
-
     # pre-allocate some matrices
-    Σ = zeros(n_obs, n_obs)
+    Σ = Symmetric(zeros(n_obs, n_obs))
     F⨉I_A⁻¹ = zeros(n_obs, n_var)
     F⨉I_A⁻¹S = zeros(n_obs, n_var)
-    I_A = similar(A_pre)
+    I_A = convert(Matrix, I - A_pre)
+    I_A = istril(I_A) ? LowerTriangular(I_A) : istriu(I_A) ? UpperTriangular(I_A) : I_A
 
     if gradient_required
         ∇A = sparse_gradient(ram_matrices.A)
@@ -133,7 +132,7 @@ function RAM(;
     if meanstructure
         MS = HasMeanStructure
         !isnothing(ram_matrices.M) || throw(ArgumentError("You set `meanstructure = true`, but your model specification contains no mean parameters."))
-        M_pre = materialize(ram_matrices.M, nan_params)
+        M_pre = materialize(ram_matrices.M, rand_params)
         ∇M = gradient_required ? sparse_gradient(ram_matrices.M) : nothing
         μ = zeros(n_obs)
     else
@@ -154,7 +153,7 @@ function RAM(;
         F⨉I_A⁻¹,
         F⨉I_A⁻¹S,
         I_A,
-        copy(I_A),
+        similar(parent(I_A)),
         ∇A,
         ∇S,
         ∇M,
@@ -177,7 +176,7 @@ function update!(
         materialize!(imply.M, imply.ram_matrices.M, params)
     end
 
-    @. imply.I_A = -imply.A
+    parent(imply.I_A) .= .-imply.A
     @view(imply.I_A[diagind(imply.I_A)]) .+= 1
 
     if is_gradient_required(targets) || is_hessian_required(targets)
@@ -189,7 +188,7 @@ function update!(
     end
 
     mul!(imply.F⨉I_A⁻¹S, imply.F⨉I_A⁻¹, imply.S)
-    mul!(imply.Σ, imply.F⨉I_A⁻¹S, imply.F⨉I_A⁻¹')
+    mul!(parent(imply.Σ), imply.F⨉I_A⁻¹S, imply.F⨉I_A⁻¹')
 
     if MeanStructure(imply) === HasMeanStructure
         mul!(imply.μ, imply.F⨉I_A⁻¹, imply.M)
@@ -215,23 +214,22 @@ end
 ### additional functions
 ############################################################################################
 
-function check_acyclic(A_pre::AbstractMatrix, A::ParamsMatrix)
-    # fill copy of A with random parameters
-    A_rand = materialize(A, rand(nparams(A)))
-
+function check_acyclic(A::AbstractMatrix)
     # check if the model is acyclic
-    acyclic = isone(det(I - A_rand))
+    acyclic = isone(det(I - A))
 
     # check if A is lower or upper triangular
-    if istril(A_rand)
+    if istril(A)
         @info "A matrix is lower triangular"
-        return LowerTriangular(A_pre)
-    elseif istriu(A_rand)
+        return LowerTriangular(A)
+    elseif istriu(A)
         @info "A matrix is upper triangular"
-        return UpperTriangular(A_pre)
-    elseif acyclic
-        @info "Your model is acyclic, specifying the A Matrix as either Upper or Lower Triangular can have great performance benefits.\n" maxlog =
-            1
-        return A_pre
+        return UpperTriangular(A)
+    else
+        if acyclic
+            @info "Your model is acyclic, specifying the A Matrix as either Upper or Lower Triangular can have great performance benefits.\n" maxlog =
+                1
+        end
+        return A
     end
 end
