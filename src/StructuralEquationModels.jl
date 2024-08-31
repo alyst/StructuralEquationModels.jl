62be0e5c3c832b206b05955788b449c35a455a06
Alexey Stukalov <astukalol@seer.bio>
2024-08-31 15:30:13 -0700

    add Xt_A_X!() method

--- src/StructuralEquationModels.jl
+++ src/StructuralEquationModels.jl
@@ -18,6 +18,7 @@ include("objective_gradient_hessian.jl")
 
 # helper objects and functions
 include("additional_functions/commutation_matrix.jl")
+include("additional_functions/quad.jl")
 
 # fitted objects
 include("frontend/fit/SemFit.jl")
---
+++ src/additional_functions/quad.jl
@@ -0,0 +1,16 @@
+# calculate Xᵀ⋅A⋅X
+# FIXME: use PDMats.jl when its sparse matrix support is refactored
+# see https://github.com/JuliaStats/PDMats.jl/pull/188
+function Xt_A_X!(res::AbstractMatrix, A::AbstractMatrix, X::AbstractMatrix,
+                 alpha::Real = 1, beta::Real = 0;
+                 A_X_buf::Union{AbstractMatrix, Nothing} = nothing)
+    A_X = !isnothing(A_X_buf) ? mul!(A_X_buf, A, X) : A * X
+    return mul!(res, X', A_X, alpha, beta)
+end
+
+function Xt_A_X!(res::AbstractMatrix, A::AbstractMatrix, X::SparseArrays.AbstractSparseMatrixCSC,
+                 alpha::Real = 1, beta::Real = 0;
+                 Xt_A_buf::Union{AbstractMatrix, Nothing} = nothing)
+    Xt_A = !isnothing(Xt_A_buf) ? mul!(Xt_A_buf, X', A) : X'A
+    return mul!(res', X', Xt_A', alpha, beta)
+end
