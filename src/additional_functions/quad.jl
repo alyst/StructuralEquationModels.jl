8b232d30e4ff6e82ec88fdd3f5f0bee1c2418c5e
Alexey Stukalov <astukalol@seer.bio>
2024-08-31 16:54:17 -0700

    add X_A_Xt!()

--- src/additional_functions/quad.jl
+++ src/additional_functions/quad.jl
@@ -21,3 +21,22 @@ Xt_A_X(A::AbstractMatrix, X::AbstractMatrix,
     Xt_A_X!(Matrix{promote_type(eltype(A), eltype(X))}(undef, size(X, 2), size(X, 2)),
             A, X, alpha, beta; A_X_buf)
 
+function X_A_Xt!(res::AbstractMatrix, A::AbstractMatrix, X::AbstractMatrix,
+                 alpha::Real = 1, beta::Real = 0;
+                 X_A_buf::Union{AbstractMatrix, Nothing} = nothing)
+    X_A = !isnothing(X_A_buf) ? mul!(X_A_buf, X, A) : X * A
+    return mul!(res, X_A, X', alpha, beta)
+end
+
+function X_A_Xt!(res::AbstractMatrix, A::AbstractMatrix, X::SparseArrays.AbstractSparseMatrixCSC,
+                 alpha::Real = 1, beta::Real = 0;
+                 X_A_buf::Union{AbstractMatrix, Nothing} = nothing)
+    X_A = !isnothing(X_A_buf) ? mul!(X_A_buf, X, A) : X * A
+    return mul!(res', X, X_A', alpha, beta)
+end
+
+X_A_Xt(A::AbstractMatrix, X::AbstractMatrix,
+       alpha::Real = 1, beta::Real = 0;
+       A_X_buf::Union{AbstractMatrix, Nothing} = nothing) =
+    X_A_Xt!(Matrix{promote_type(eltype(A), eltype(X))}(undef, size(X, 1), size(X, 1)),
+            A, X, alpha, beta; A_X_buf)
