0732ed7344db748b35e68a7889e81a5c5e3ab1f1
Alexey Stukalov <astukalol@seer.bio>
2024-10-08 12:51:51 -0700

    quad: fixup kwargs passing

--- src/additional_functions/quad.jl
+++ src/additional_functions/quad.jl
@@ -112,10 +112,9 @@ function Xt_A_X!(res::AbstractMatrix, A::Symmetric{<:Any, M}, X::AbstractMatrix,
 end
 
 Xt_A_X(A::AbstractMatrix, X::AbstractMatrix,
-       alpha::Real = 1, beta::Real = 0;
-       A_X_buf::Union{AbstractMatrix, Nothing} = nothing) =
+       alpha::Real = 1, beta::Real = 0; kwargs...) =
     Xt_A_X!(Matrix{promote_type(eltype(A), eltype(X))}(undef, size(X, 2), size(X, 2)),
-            A, X, alpha, beta; A_X_buf)
+            A, X, alpha, beta; kwargs...)
 
 function X_A_Xt!(res::AbstractMatrix, A::AbstractMatrix, X::AbstractMatrix,
                  alpha::Real = 1, beta::Real = 0;
@@ -138,6 +137,6 @@ end
 
 X_A_Xt(A::AbstractMatrix, X::AbstractMatrix,
        alpha::Real = 1, beta::Real = 0;
-       X_A_buf::Union{AbstractMatrix, Nothing} = nothing) =
+       kwargs...) =
     X_A_Xt!(Matrix{promote_type(eltype(A), eltype(X))}(undef, size(X, 1), size(X, 1)),
-            A, X, alpha, beta; X_A_buf)
+            A, X, alpha, beta; kwargs...)
